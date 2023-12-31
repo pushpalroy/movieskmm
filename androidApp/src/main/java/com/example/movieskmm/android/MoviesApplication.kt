c38db2bc09182ab13671bb5ae1111bbc027d41f5
Pushpal Roy <pushpalroy2007@gmail.com>
2023-12-30 18:15:39 -0600

    add android logger for koin

--- androidApp/src/main/java/com/example/movieskmm/android/MoviesApplication.kt
+++ androidApp/src/main/java/com/example/movieskmm/android/MoviesApplication.kt
@@ -3,11 +3,13 @@ package com.example.movieskmm.android
 import android.app.Application
 import com.example.movieskmm.di.initKoin
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 
 class MoviesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initKoin {
+            androidLogger()
             androidContext(this@MoviesApplication)
         }
     }
--- shared/src/commonMain/kotlin/com/example/movieskmm/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/example/movieskmm/di/Koin.kt
@@ -24,7 +24,7 @@ fun initKoin(declaration: KoinAppDeclaration = {}) =
         )
     }
 
-fun initKoin() = initKoin() {}
+fun initKoin() = initKoin {}
 
 val repoModule = module {
     single<MoviesRepo> {
