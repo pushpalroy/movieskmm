392085e3777b9409ddbb9d2d05dbcf99b66077c7
Pushpal Roy <pushpalroy2007@gmail.com>
2024-01-11 05:38:01 -0600

    [test] add a common dao test

--- shared/src/commonMain/kotlin/com/example/movieskmm/data/local/db/DatabaseSchema.kt
+++ shared/src/commonMain/kotlin/com/example/movieskmm/data/local/db/DatabaseSchema.kt
@@ -1,7 +1,56 @@
 package com.example.movieskmm.data.local.db
 
 import app.cash.sqldelight.db.SqlDriver
+import data.local.db.AppDatabaseQueries
+import data.local.db.Favourite_movie
 
 fun createQueryWrapper(sqlDriver: SqlDriver): AppDatabase {
     return AppDatabase(driver = sqlDriver)
+}
+
+object TestSchema {
+
+    val firstMovie = Favourite_movie(
+        id = 1,
+        title = "first movie from test schema",
+        overview = "first overview",
+        backdropPath = "first backdropPath",
+        posterPath = "first posterPath",
+        voteAverage = "1.1"
+    )
+    val secondMovie = Favourite_movie(
+        id = 2,
+        title = "second movie from test schema",
+        overview = "second overview",
+        backdropPath = "second backdropPath",
+        posterPath = "second posterPath",
+        voteAverage = "1.2"
+    )
+    val thirdMovie = Favourite_movie(
+        id = 3,
+        title = "third movie from test schema",
+        overview = "third overview",
+        backdropPath = "third backdropPath",
+        posterPath = "third posterPath",
+        voteAverage = "1.3"
+    )
+    val movies = listOf(firstMovie, secondMovie, thirdMovie)
+
+    fun insertTestMovies(appDatabaseQueries: AppDatabaseQueries) =
+        movies.forEach { localMovie ->
+            appDatabaseQueries.insetEachMovie(localMovie)
+        }
+
+    fun AppDatabaseQueries.insetEachMovie(favouriteMovie: Favourite_movie) {
+        with(favouriteMovie) {
+            insertMovie(
+                id,
+                title,
+                overview,
+                backdropPath,
+                posterPath,
+                voteAverage.toString()
+            )
+        }
+    }
 }
---
+++ shared/src/commonTest/kotlin/com/example/movieskmm/domain/usecase/security/AppDatabaseDaoTest.kt
@@ -0,0 +1,63 @@
+package com.example.movieskmm.domain.usecase.security
+
+import com.example.movieskmm.BaseTest
+import com.example.movieskmm.data.local.db.TestSchema
+import com.example.movieskmm.data.local.db.TestSchema.insetEachMovie
+import com.example.movieskmm.data.local.entity.LocalMovieEntity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@ExperimentalCoroutinesApi
+class AppDatabaseDaoTest : BaseTest() {
+
+    private val appDatabaseDAO = dbHelper.appDatabaseDAO
+
+    private val movies =
+        listOf(TestSchema.firstMovie, TestSchema.secondMovie, TestSchema.thirdMovie)
+
+    @BeforeTest
+    fun setUp() = runTest {
+        val appDatabase = dbHelper.buildDbIfNeed().appDatabase
+        appDatabase.appDatabaseQueries.transaction {
+            movies.forEach {
+                appDatabase.appDatabaseQueries.insetEachMovie(it)
+            }
+        }
+    }
+
+    @AfterTest
+    fun tearDown() = runTest {
+        val appDatabase = dbHelper.buildDbIfNeed().appDatabase
+        appDatabase.appDatabaseQueries.transaction {
+            movies.forEach {
+                appDatabase.appDatabaseQueries.deleteMovieById(it.id)
+            }
+        }
+    }
+
+    @Test
+    fun should_fetch_all_inserted_movies() = runTest {
+        assertEquals(movies, appDatabaseDAO.getAllFavMovies().first())
+    }
+
+    @Test
+    fun should_insert_new_item_correctly() = runTest {
+        appDatabaseDAO.insert(
+            LocalMovieEntity(
+                4, "Abc", "Xyz", "/abc", "/xyz", 1.1
+            )
+        )
+        val favouriteMovie = appDatabaseDAO.getAllFavMovies().first().last()
+        assertEquals(4, favouriteMovie.id)
+        assertEquals("Abc", favouriteMovie.title)
+        assertEquals("Xyz", favouriteMovie.overview)
+        assertEquals("/abc", favouriteMovie.backdropPath)
+        assertEquals("/xyz", favouriteMovie.posterPath)
+        assertEquals("1.1", favouriteMovie.voteAverage)
+    }
+}
