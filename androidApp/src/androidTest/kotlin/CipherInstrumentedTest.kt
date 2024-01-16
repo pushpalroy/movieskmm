73a96a2fb3877ad2d3f001c1e74bf8d34e051310
Pushpal Roy <pushpalroy2007@gmail.com>
2024-01-16 10:21:30 -0600

    [test] add ios cipher tests

--- androidApp/src/androidTest/kotlin/CipherInstrumentedTest.kt
+++ androidApp/src/androidTest/kotlin/CipherInstrumentedTest.kt
@@ -24,7 +24,7 @@ import org.junit.runner.RunWith
 
 @MediumTest
 @RunWith(AndroidJUnit4::class)
-class SqlDelightCipherInstrumentedTest {
+class CipherInstrumentedTest {
 
     private val context = ApplicationProvider.getApplicationContext<Context>()
 
---
+++ shared/src/iosTest/kotlin/com/example/movieskmm/data/local/db/IosCipherUtilsTest.kt
@@ -0,0 +1,66 @@
+package com.example.movieskmm.data.local.db
+
+import com.example.movieskmm.BaseTest
+import com.example.movieskmm.data.local.db.DbHelper.Companion.DB_NAME
+import com.example.movieskmm.data.local.db.util.PlatformSQLiteState
+import com.example.movieskmm.domain.usecase.security.CheckEncryptionPassphraseUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import platform.Foundation.NSFileManager
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@ExperimentalCoroutinesApi
+class IosCipherUtilsTest : BaseTest() {
+
+    @Test
+    fun getDatabaseStateTest() {
+        IosCipherUtils.deleteDatabase()
+        var exp = PlatformSQLiteState.DOES_NOT_EXIST
+        var act = IosCipherUtils.getDatabaseState(DB_NAME)
+        assertEquals(exp, act)
+
+        IosDatabaseHolder().close()
+        exp = PlatformSQLiteState.DOES_NOT_EXIST
+        act = IosCipherUtils.getDatabaseState(DB_NAME)
+        assertEquals(exp, act)
+
+        IosCipherUtils.deleteDatabase()
+        IosDatabaseHolder(key = "password").close()
+        exp = PlatformSQLiteState.DOES_NOT_EXIST
+        act = IosCipherUtils.getDatabaseState(DB_NAME)
+        assertEquals(exp, act)
+    }
+
+    @Test
+    @Ignore
+    fun checkKeyTest() = runTest {
+        IosCipherUtils.deleteDatabase()
+        IosDatabaseHolder().close()
+
+        var exp = true
+        var act = IosCipherUtils.checkKey(null, DB_NAME)
+        assertEquals(exp, act, "null pass")
+
+        IosCipherUtils.deleteDatabase()
+        IosDatabaseHolder(key = "password").close()
+
+        exp = false
+        act = IosCipherUtils.checkKey("incorrect password", DB_NAME)
+        assertEquals(exp, act, "incorrect pass")
+
+        exp = true
+        val checkEncryptionPassphraseUseCase = CheckEncryptionPassphraseUseCase(dbHelper)
+        act = checkEncryptionPassphraseUseCase.perform("password")
+        assertEquals(exp, act, "correct pass")
+    }
+
+    @Test
+    fun deleteDatabaseTest() {
+        val dbPath = IosCipherUtils.getDatabasePath(DB_NAME)
+        val exp = NSFileManager.defaultManager.fileExistsAtPath(dbPath)
+        val act = IosCipherUtils.deleteDatabase()
+        assertEquals(exp, act)
+    }
+}
